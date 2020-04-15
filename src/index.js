6c01a37f783e33d98a7c04268cad66d5ff8642d9
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-04-15 16:35:54 -0600

    added login-userpage photo

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/hotel-main.jpg'
+import './images/user-login-picture.jpg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
