65b7b7da7e1e9f7e586a7b4675728fbcd7d7268c
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-04-17 11:23:42 -0600

    :crystal_ball: Completed filter user-test

--- src/User.js
+++ src/User.js
@@ -2,19 +2,13 @@ class User {
     constructor(user, bookingsData) {
         this.id = user.id;
         this.name = user.name;
-        this.myBookings = this.findMyBookings(user, bookingsData)
+        this.myBookings = bookingsData.allBookings.filter(booking => booking.userID===user.id)
     }
 
-    findMyBookings(user, bookingsData) {
-        const userBookings = []
-        bookingsData.allBookings.forEach(element => {
-            if(user.id === element.userID) {
-                userBookings.push(element)
-            }
-        })
-        console.log('userBookings', userBookings)
-        return userBookings
-    }
+    // createBooking(){
+
+    // }
+
 }
 
 export default User;
--- test/user-test.js
+++ test/user-test.js
@@ -2,15 +2,15 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import User from '../src/User';
+// import Bookings from '../src/Bookings';
 
 describe('User', function() {
   let user1;
+  let bookingsData;
 
   beforeEach(() => {
-    user1 = new User({
-      'id': 34,
-      'name': 'Luisa Ardella Jakubowski',
-      'myBookings': [{
+    bookingsData = {
+      'allBookings': [{
         'id': '5fwrgu4i7k55hl6tg',
         'userID': 34,
         'date': '2020/02/03',
@@ -22,8 +22,20 @@ describe('User', function() {
         'date': '2020/02/05',
         'roomNumber': 14,
         'roomService': [],
-      }]
-    })
+      }, {
+        'id': '5fwrgu4i7k55hl6wp',
+        'userID': 50,
+        'date': '2020/02/05',
+        'roomNumber': 14,
+        'roomService': [],
+      }],
+     
+    }
+    user1 = new User({
+      'id': 34,
+      'name': 'Luisa Ardella Jakubowski'
+    }, bookingsData)
+
   })
   it.only('should be a function', function() {
     expect(User).to.be.a('function');
@@ -35,7 +47,6 @@ describe('User', function() {
     expect(user1.name).to.eq("Luisa Ardella Jakubowski")
   });
   it.only('should have an array of rooms booked', function() {
-    // user1.findMyBookings()
     expect(user1.myBookings).to.deep.equal([{
       'id': '5fwrgu4i7k55hl6tg',
       'userID': 34,
